f8e9e393a702ef0de2d34210aa29136747a09504
Raisa Moreno <moreno.raisa.cursos@gmail.com>
2023-10-25 02:07:02 -0300

    Requisito 2 parcialmente correto

--- src/functions.js
+++ src/functions.js
@@ -12,6 +12,19 @@ verifyPalindrome('desenvolvimento');
 
 // Requisito 2 - Crie a função getHighestIndex
 
+function getHighestIndex(array) {
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] > array[index - 1]) {
+      highestNumber = array[index];
+    }
+  }
+  return array.indexOf(highestNumber);
+}
+
+getHighestIndex([2, 3, 6, 7, 10, 1]);
+getHighestIndex([9, 1, 3, 5, 7]);
+getHighestIndex([-9, -1, -3, -5, -7]);
+
 // Requisito 3 - Crie a função getSmallestIndex
 
 // Requisito 4 - Crie a função getLongestWord
@@ -20,11 +33,11 @@ verifyPalindrome('desenvolvimento');
 
 // Não modifique as linhas abaixo
 module.exports = {
-  verifyPalindrome: typeof verifyPalindrome === 'function' ? verifyPalindrome : (() => {}),
-  getHighestIndex: typeof getHighestIndex === 'function' ? getHighestIndex : (() => {}),
-  getSmallestIndex: typeof getSmallestIndex === 'function' ? getSmallestIndex : (() => {}),
-  getLongestWord: typeof getLongestWord === 'function' ? getLongestWord : (() => {}),
+  verifyPalindrome: typeof verifyPalindrome === 'function' ? verifyPalindrome : (() => { }),
+  getHighestIndex: typeof getHighestIndex === 'function' ? getHighestIndex : (() => { }),
+  getSmallestIndex: typeof getSmallestIndex === 'function' ? getSmallestIndex : (() => { }),
+  getLongestWord: typeof getLongestWord === 'function' ? getLongestWord : (() => { }),
   countHighestNumberMaxOccurrences: typeof countHighestNumberMaxOccurrences === 'function'
     ? countHighestNumberMaxOccurrences
-    : (() => {}),
+    : (() => { }),
 };
